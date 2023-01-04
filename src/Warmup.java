ea461dba50b43ba9b2d4faf906cb63bd701b4fe6
MariaMirzhanova8712 <marija-mirzhanva@rambler.ru>
2023-01-04 20:07:44 +0400

    add task

---
+++ src/Warmup.java
@@ -0,0 +1,28 @@
+public class Warmup {
+
+    public static void main(String[] args) {
+        arrayCount9(1, 9, 1, 2, 9); // 1
+        arrayCount9(1, 9, 1, 9, 9); // 2
+        arrayCount9(1, 9, 9, 3, 9); // 3
+
+    }
+
+    private static void arrayCount9(int i, int i1, int i2, int i3, int i4) {
+    }
+
+    //private static void arrayCount9(int i4, int i3, int i, int i1, int i2) {
+    //}
+
+    public static int arrayCount9(int[] nums) {
+
+        int count = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 9) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+
+}
